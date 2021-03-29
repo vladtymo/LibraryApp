c94e56fcf39afdf7ea2f6550b05aadc413703a14
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2021-03-29 11:48:30 +0300

    Move View Models to Separate Folder

---
+++ PresentationLayer/ViewModels/BookViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace PresentationLayer
+{
+    public class BookViewModel : ViewModelBase
+    {
+        private string title;
+        private int pages;
+        private string genreName;
+        private AuthorViewModel author;
+
+        public int Id { get; set; }
+
+        public string Title { get => title; set => SetProperty(ref title, value); }
+        public int Pages { get => pages; set => SetProperty(ref pages, value); }
+        public string GenreName { get => genreName; set => SetProperty(ref genreName, value); }
+
+        public AuthorViewModel Author { get => author; set => SetProperty(ref author, value); }
+    }
+}
---
+++ PresentationLayer/ViewModels/MainViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using AutoMapper;
+using BusinessLogicLayer;
+
+namespace PresentationLayer
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private IBookService bookService = new BookService();
+        private IMapper mapper;
+
+        private ICollection<BookViewModel> books = new ObservableCollection<BookViewModel>();
+        private BookViewModel selectedBook;
+
+        public MainViewModel()
+        {
+            IConfigurationProvider config = new MapperConfiguration(
+                cfg =>
+                {
+                    cfg.CreateMap<BookDTO, BookViewModel>();
+                    cfg.CreateMap<AuthorDTO, AuthorViewModel>();
+
+                    cfg.CreateMap<BookViewModel, BookDTO>();
+                    cfg.CreateMap<AuthorViewModel, AuthorDTO>();
+                });
+
+            mapper = new Mapper(config);
+
+            LoadAllBooks();
+        }
+
+        public void LoadAllBooks()
+        {
+            var result = mapper.Map<IEnumerable<BookViewModel>>(bookService.GetAllBooks());
+
+            books.Clear();
+            foreach (var b in result)
+            {
+                books.Add(b);
+            }
+        }
+
+        // Binding Properties
+        public IEnumerable<BookViewModel> Books => books;
+        public BookViewModel SelectedBook
+        {
+            get { return selectedBook; }
+            set { SetProperty(ref selectedBook, value); }
+        }
+    }
+}
